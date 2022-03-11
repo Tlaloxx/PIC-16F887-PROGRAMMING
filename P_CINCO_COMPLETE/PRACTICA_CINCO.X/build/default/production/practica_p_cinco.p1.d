2010d5f9fca86c364f10a4827a5d21adf3a4f364
Tlaloc Alvarez <67183059+Tlaloxx@users.noreply.github.com>
2022-03-11 13:31:06 -0800

    Add files via upload

---
+++ P_CINCO_COMPLETE/PRACTICA_CINCO.X/build/default/production/practica_p_cinco.p1.d
@@ -0,0 +1,2 @@
+build/default/production/practica_p_cinco.p1:  \
+practica_p_cinco.c 
---
+++ P_CINCO_COMPLETE/PRACTICA_CINCO.X/practica_p_cinco.c
@@ -0,0 +1,37 @@
+// PIC16F887 Configuration Bit Settings
+// SAUCEDA ALVAREZ JOSE LUIS TLALOC
+
+// CONFIG1
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator: High-speed crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = ON        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+#define _XTAL_FREQ 4000000 
+
+unsigned char cuenta;
+unsigned char tabla[] = {0x77, 0x41, 0x3B, 0x6B, 0x4D, 0x6E, 0x7E, 0x43, 0x7F, 0x6F};
+
+void main(void) {
+    TRISB = 0x00;                       //CONFIGURACION DE PUERTO B COMO SALIDA
+    while(1){
+        for(cuenta = 0; cuenta!= 10; cuenta++){
+            PORTB = tabla[cuenta];      //DECODIFICACION DE BITS
+            __delay_ms(500);
+        }
+    }
+}
+
+
+                
